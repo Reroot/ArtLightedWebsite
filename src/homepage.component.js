f33bd6dd06b8f9f76baa67741dbcbd4d1f968421
Artem <triple8six@gmail.com>
2020-04-13 04:59:14 -0400

    first commit

---
+++ src/homepage.component.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+<div>
+<div>
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='title'>DIGITAL ART
+                    <div className='subtitle'>VIEW </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='title'>PHOTOGRAPHY
+                    <div className='subtitle'>SHOP NOW </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='title'>SKETCHES
+                    <div className='subtitle'>SHOP NOW </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='title'>WRITINGS
+                    <div className='subtitle'>SHOP NOW </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='title'>OIL PAINTINGS
+                    <div className='subtitle'>SHOP NOW </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='title'>ABSTRACTIONS
+                    <div className='subtitle'>SHOP NOW </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+);
+
+export default HomePage;
